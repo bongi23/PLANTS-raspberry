d7b1bfbb5170a240ff1b51ca1bf92f8fd430c088
Matteo Franceschi <mfranceschi94@gmail.com>
2019-05-20 15:21:19 +0200

    refactor

--- application_layer.py
+++ application_layer.py
@@ -1,45 +1,49 @@
 from serial_manager import SerialManager
 from serialprotocol import DATA
 import time
+from network_layer_serial_manager import NetworkLayerSerialManager
 import network_manager
 import asyncio
 import socket
+from aiohttp import web, http_exceptions
 
 COMPONENT_ID = 50
 SENSING_RESP = 1
-SENSING_REQ = 2
-NEW_SAMPLE = 3
-NEW_PLANT = 4
-DISCONNECTED_PLANT = 5
+REMOVE_SENSING_REQ = 4
+REMOVE_SENSING_RESP = 2
+SENSING_REQ = 3
+NEW_SAMPLE = 5
+NEW_PLANT = 6
+DISCONNECTED_PLANT = 7
 
 '''
-    sensing message id 2
+    sensing message id 3
 
     uint32 microbit_id
+    uint32 gradient_id
     string sensor_name
-    byte sample_rate
     byte min_val
     byte max_val
     [
-        uint32_t sample_rate,
         uint32_t min_val,
         uint32_t max_val
     ]
 
-    response id 1
+    response id 1 or 2
 
+    uint32_t gradient_id
     byte 
         0 -> fine
         1 -> no route to microbit
         2 -> microbit disconnected
 
-    new_sample id 3
+    new_sample id 5
 
     uint32 microbit_id
     string sensor_name
     float value
 
-    new_plant id 4
+    new_plant id 6
 
     uint32 microbit_id
     char [][] sensors
@@ -50,16 +54,88 @@ DISCONNECTED_PLANT = 5
 
 '''
 
-class ApplicationLayer:
+def check_int(t, name):
+    try:
+        t = int(t)
+    except Exception:
+        raise http_exceptions.HttpBadRequest('{0} must be an integer'.format(name))
+    if t < 0:
+        raise http_exceptions.HttpBadRequest('{0} must be unsigned'.format(name))
+    if t.bit_length() > 32:
+        raise http_exceptions.HttpBadRequest("{0} can't be more than 32 bit long".format(name))
+
+routes = web.RouteTableDef()
+
+class EventHandler:
+
+    def __init__(self, microbit_id, min_value=None, max_value=None):
+        self.microbit_id = microbit_id
+        self.min_value = min_value
+        self.max_value = max_value
+
+@routes.view('/sensing/{microbit_id}/{event_id}')
+class ApplicationLayer(web.View):
 
     def __init__(self):
         self.__serial = None
+        self.__events = {}
+        self.__network_layer = None
+        super(ApplicationLayer, self).__init__()
+
+    async def put(self):
+        request = self.request
+        microbit_id = request.match_info['microbit_id']
+        check_int(microbit_id, 'microbit_id')
+        microbit_id = int(microbit_id)
+        min_value = request.query.get('min_value', None)
+        if min_value is not None:
+            check_int(min_value, 'min_value')
+        min_value = int(min_value)
+        max_value = request.query.get('max_value', None)
+        if max_value is not None:
+            check_int(max_value, 'max_value')
+        max_value = int(max_value)
+
+        event_id = request.match_info['event_id']
+        if len(self.__network_layer.routing_table[microbit_id]) == 0:
+            return web.Response(status=404)
+
+        self.__events[event_id] =  EventHandler(microbit_id, min_value, max_value)
+
+        return web.Response()
+
+    async def delete(self):
+        request = self.request
+        microbit_id = request.match_info['microbit_id']
+        check_int(microbit_id, 'microbit_id')
+        microbit_id = int(microbit_id)
+        event_id = request.match_info['event_id']
+        event_id = int(event_id)
+        self.__events.pop(event_id)
+        return web.Response()
 
 
+    @routes.put('/sensing/{microbit_id}/{sensor_name}/time')
+    async def update_sample_rate(self, request):
+        microbit_id = request.match_info['microbit_id']
+        check_int(microbit_id, 'microbit_id')
+        sensor_name = request.match_info['sensor_name']
+        sample_rate = request.query.get('sample_rate')
+        check_int(sample_rate, 'sample_rate')
 
+        # resp = await self.__serial.recv_send()
+        return web.Response()
 
-    def __call__(self, serial: SerialManager):
+    def __call__(self, serial: SerialManager, nl: NetworkLayerSerialManager):
         self.__serial = serial
+        self.__network_layer = nl
+
+        app = web.Application()
+        app.add_routes(routes)
+        runner = web.AppRunner(app)
+        await runner.setup()
+        site = web.TCPSite(runner, 'localhost', 8080)
+        await site.start()
 
         @serial.listen(COMPONENT_ID, NEW_SAMPLE, full_payload=True)
         def new_sample(payload):
--- network_manager.py
+++ network_manager.py
@@ -6,57 +6,11 @@ routes = web.RouteTableDef()
 
 COMPONENT_ID = 50
 SENSING_RESP = 1
-SENSING_REQ = 2
-
-serial = None
-
-
-
-def check_int(t, name):
-    try:
-        t = int(t)
-    except Exception:
-        raise http_exceptions.HttpBadRequest('{0} must be an integer'.format(name))
-    if t < 0:
-        raise http_exceptions.HttpBadRequest('{0} must be unsigned'.format(name))
-    if t.bit_length() > 32:
-        raise http_exceptions.HttpBadRequest("{0} can't be more than 32 bit long".format(name))
+SENSING_REQ = 3
+REMOVE_SENSING_REQ = 4
+REMOVE_SENSING_RESP = 2
     
 
-@routes.put('/sensing/{microbit_id}/{sensor_name}')
-async def sensing_req(request):
-    microbit_id = request.match_info['microbit_id']
-    check_int(microbit_id, 'microbit_id')
-    sampling_time = request.query.get('sampling_time', None)
-    if sampling_time is not None:
-        check_int(sampling_time,'sampling_time')
-    min_value = request.query.get('min_value', None)
-    if min_value is not None:
-        check_int(min_value, 'min_value')
-    max_value = request.query.get('max_value', None)
-    if max_value is not None:
-        check_int(max_value, 'max_value')
-
-    sensor_name = request.match_info['sensor_name']
-    
-    resp = await app_layer.send_sensing(microbit_id, sensor_name, sampling_time, min_value, max_value)
-    if resp == 0:
-        return web.Response()
-    elif resp == 1:
-        return web.Response(status=404)
-    elif resp == 2:
-        return web.Response(status=410)
-
-
-async def start_server(s: SerialManager):
-    global serial
-    serial = s
-    app = web.Application()
-    app.add_routes(routes)
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.TCPSite(runner, 'localhost', 8080)
-    await site.start()
 
 
 
@@ -72,16 +26,12 @@ async def send_put(url, url_args=None, params=None, json=None):
             return resp
 
 
-async def send_sensing(serial, microbit_id, sensor_name, sample_rate=None, min_val=None, max_val=None):
+async def send_sensing(serial, microbit_id, sensor_name, min_val=None, max_val=None):
     #TODO: create message
     msg = DATA(COMPONENT_ID, SENSING_REQ)
 
     msg += microbit_id
     msg += sensor_name
-    if sample_rate is not None:
-        msg += ('B', 1)
-    else:
-        msg += ('B', 0)
     if min_val is not None:
         msg += ('B', 1)
     else:
@@ -90,8 +40,6 @@ async def send_sensing(serial, microbit_id, sensor_name, sample_rate=None, min_v
         msg += ('B', 1)
     else:
         msg += ('B', 0)
-    if sample_rate is not None:
-        msg += sample_rate
     if min_val is not None:
         msg += min_val
     if max_val is not None:
@@ -99,7 +47,7 @@ async def send_sensing(serial, microbit_id, sensor_name, sample_rate=None, min_v
 
     resp = DATA(COMPONENT_ID, SENSING_RESP)
     resp *= 'B'
-    resp(await serial.recv_send(payload=bytes(msg), full_payload=True))
+    resp(await serial.recv_send(COMPONENT_ID, SENSING_RESP, payload=bytes(msg), full_payload=True))
 
     return resp.get_data()[0]
 
